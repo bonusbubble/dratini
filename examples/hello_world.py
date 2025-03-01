fe1b1607ea7d330bf6dbe1cd905286c5370936ac
bonusbubble <22643122+bonusbubble@users.noreply.github.com>
2025-03-01 02:34:16 -0600

    Update hello_world.py

--- examples/hello_world.py
+++ examples/hello_world.py
@@ -1,14 +1,15 @@
-screen_width: int = 800
-screen_height: int = 450
+screen_width: auto = 640
+screen_height: auto = 480
+screen_title: auto = "Dratini App"
 
-InitWindow(screen_width, screen_height, "Dratini App")
+init_window(screen_width, screen_height, "Dratini App")
 
-SetTargetFPS(60)
+set_target_fps(60)
 
-while not WindowShouldClose():
-    BeginDrawing()
-    ClearBackground(BLACK)
-    DrawText("Hello, Dratini!", 190, 200, 20, PINK)
-    EndDrawing()
+while not window_should_close():
+    begin_drawing()
+    clear_background(BLACK)
+    draw_text("Hello, Dratini!", screen_width / 2, screen_height / 2, 20, PINK)
+    end_drawing()
 
-CloseWindow()
+close_window()
