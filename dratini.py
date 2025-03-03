918ee2db91521bd1c7ef10626310debd2cc5dc83
bonusbubble <22643122+bonusbubble@users.noreply.github.com>
2025-03-03 05:29:18 -0600

    Make dratini.py importable

--- dratini.py
+++ dratini.py
@@ -3,25 +3,26 @@
 import argparse
 from ast import parse as _parse_python
 from dratini.code_generation.cpp import generate_cpp
-from dratini.utils import load_text_files_as_one, parse_program_arguments, print_dump, save_text_file, throw_feature_not_supported
+from dratini.utils import load_text_files_as_one as _load_text_files_as_one, parse_program_arguments as _parse_program_arguments, print_dump as _print_dump, save_text_file as _save_text_file, throw_feature_not_supported as _throw_feature_not_supported
 import os
 from random import randint
 import subprocess
 from tempfile import TemporaryDirectory
 
 
-__dir__ = os.path.dirname(os.path.abspath(__file__))
+# Get the path to this script.
+_SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 
-LINKING_DIR = os.path.join(__dir__, "dratini", "linking")
-INCLUDE_DIR = os.path.join(LINKING_DIR, "include")
-LIB_DIR = os.path.join(LINKING_DIR, "lib")
+_LINKING_DIR = os.path.join(_SCRIPT_DIR, "dratini", "linking")
+_INCLUDE_DIR = os.path.join(_LINKING_DIR, "include")
+_LIB_DIR = os.path.join(_LINKING_DIR, "lib")
 
 
 def _add_dratini_base_flags_to_cxx_command(command: list[str], args: argparse.Namespace) -> list[str]:
     command.append("-std=c++17")
-    command.append("-I" + INCLUDE_DIR)
+    command.append("-I" + _INCLUDE_DIR)
     if not args.emit_asm and not args.emit_obj:
-        command.append("-L" + LIB_DIR)
+        command.append("-L" + _LIB_DIR)
         command.append("-l:libbgc.a")
         command.append("-l:libraylib.a")
     return command
@@ -39,7 +40,7 @@ def _add_dratini_inherited_flags_to_cxx_command(command: list[str], args: argpar
     return command
 
 
-def get_cxx_flags(
+def _get_cxx_flags(
         args: argparse.Namespace
 ) -> str:
     flags = []
@@ -48,7 +49,7 @@ def get_cxx_flags(
     return flags
 
 
-def cxx(
+def _cxx(
         cxx: str="clang++",
         input_source_code: str=None,
         input_path: str=None,
@@ -70,7 +71,7 @@ def cxx(
         else:
             command.append("-xc++")
             command.append("-")
-        cxx_flags = get_cxx_flags(args)
+        cxx_flags = _get_cxx_flags(args)
         command.extend(cxx_flags)
         command.insert(0, cxx)
         # print(" ".join(command))
@@ -79,47 +80,52 @@ def cxx(
     return result
 
 
-# Parse the arguments passed to the program.
-args = parse_program_arguments()
+def _main():
+    # Parse the arguments passed to the program.
+    args = _parse_program_arguments()
 
-# If the program should print the C++ compiler flags:
-if args.emit_cxxflags:
-    cxx_flags = get_cxx_flags(args)
-    print(" ".join(cxx_flags))
-    exit()
+    # If the program should print the C++ compiler flags:
+    if args.emit_cxxflags:
+        cxx_flags = _get_cxx_flags(args)
+        print(" ".join(cxx_flags))
+        exit()
 
-# Get the source code to parse.
-python_source_code = load_text_files_as_one(args.input)
+    # Get the source code to parse.
+    python_source_code = _load_text_files_as_one(args.input)
 
-# Parse the source code into an AST.
-python_module = _parse_python(python_source_code)
+    # Parse the source code into an AST.
+    python_module = _parse_python(python_source_code)
 
-# If the program should print the AST:
-if args.emit_ast:
-    # Print the AST.
-    print_dump(python_module)
-    # Exit the program.
-    exit()
+    # If the program should print the AST:
+    if args.emit_ast:
+        # Print the AST.
+        _print_dump(python_module)
+        # Exit the program.
+        exit()
 
-# Generate C++ source code from the Python module.
-cpp_source_code = generate_cpp(python_module)
+    # Generate C++ source code from the Python module.
+    cpp_source_code = _generate_cpp(python_module)
 
-# If the program should print the AST:
-if args.emit_cpp:
-    # If an output file was not specified:
-    if args.output == None or len(args.output) < 1:
-        # Write the C++ source code to the standard output stream (STDOUT).
-        print(cpp_source_code)
-    # Otherwise:
-    else:
-        # Write the C++ source code to the output file.
-        save_text_file(args.output, cpp_source_code)
-    # Exit the program.
-    exit()
+    # If the program should print the AST:
+    if args.emit_cpp:
+        # If an output file was not specified:
+        if args.output == None or len(args.output) < 1:
+            # Write the C++ source code to the standard output stream (STDOUT).
+            print(cpp_source_code)
+        # Otherwise:
+        else:
+            # Write the C++ source code to the output file.
+            _save_text_file(args.output, cpp_source_code)
+        # Exit the program.
+        exit()
 
-# Compile the generated C++ source code into machine code.
-cxx(
-        input_source_code=cpp_source_code,
-        output_path=args.output,
-        args=args
-)
+    # Compile the generated C++ source code into machine code.
+    _cxx(
+            input_source_code=cpp_source_code,
+            output_path=args.output,
+            args=args
+    )
+
+
+if __name__ == "__main__":
+    _main()
