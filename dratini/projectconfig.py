cd176c33351270c7817246c736a94d77c1674d45
bonusbubble <22643122+bonusbubble@users.noreply.github.com>
2025-03-03 03:35:10 -0600

    Merge projectconfig.py

--- dratini/projectconfig.py
+++ dratini/projectconfig.py
@@ -1,10 +1,21 @@
-import platform
-from projectconfig import *
+from .platform import *
 
 
-class Platform:
+class ProjectConfig:
     @property
-    def name(self):
-        return platform.system()
+    def version_tag(self) -> str:
+        return "v" + self.version
 
-PLATFORM = Platform()
+    def __init__(self, name: str="project", version: str = "0.0.0", description: str=""):
+        self.name = name
+        self.version = version
+        self.description = description
+
+
+PROJECT = ProjectConfig()
+
+PROJECT.name = "dratini"
+PROJECT.version = "0.0.0"
+PROJECT.brief = "Dratini AOT Compiler"
+PROJECT.description = ''''''
+PROJECT.copyright = "Copyright (c) 2025 bonusbyte.org"
