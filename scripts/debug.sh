b93dab4d8913eb8614ffe5252dcbaae8ff813605
bonusbubble <22643122+bonusbubble@users.noreply.github.com>
2025-03-03 20:26:47 -0600

    Improve debug script

--- scripts/debug.sh
+++ scripts/debug.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# ./scripts/build-generic.sh
+
 bin="examples/hello_world"
 src="$bin.py"
 cpp_src="$bin.cpp"
@@ -7,7 +9,7 @@ cpp_src="$bin.cpp"
 clear
 sudo python3 installer.py --install
 rm -f $cpp_src
-python dratini.py $src -o $cpp_src
+python dratini.py $src -o $cpp_src --cxxflags=-O3
 rm -f $bin
-python dratini.py $src -o $bin
+python dratini.py $src -o $bin --cxxflags=-O3
 ./$bin
